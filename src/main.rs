10105760ff2a396278574f93eae1d4f0c41dae61
sushimampfer <sushimampfer@duck.com>
2025-08-07 22:32:15 +0200

    database added

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 mod session;
 
+use std::sync::LazyLock;
+
 use aes_gcm::{aead::{consts::U12, OsRng}, AeadCore, Aes256Gcm, KeyInit, Nonce};
+use axum::Router;
 use once_cell::sync::Lazy;
+use sqlx::SqlitePool;
+use tokio::net::TcpListener;
 
 use crate::session::{session_to_user, user_to_session};
 
+static DB: LazyLock<SqlitePool> = LazyLock::new(|| SqlitePool::connect_lazy("sqlite://db.sqlit").unwrap());
 
 static CIPHER: Lazy<Aes256Gcm> = Lazy::new(|| Aes256Gcm::new(&Aes256Gcm::generate_key(OsRng)));
 static NONCE: Lazy<Nonce<U12>> = Lazy::new(|| Aes256Gcm::generate_nonce(OsRng));
 
-fn main() {
-    let ip1 = String::from("0000:0000:0000:0000:0000:0000:0000:0000");
-    let ip2 = String::from("127.0.0.2");
-    let username = String::from("asklhdflashfalksjdfhlakhsdlfkjhasdfka");
-    let session = user_to_session(username, ip1.clone()).unwrap();
-    println!("{}", session);
-    dbg!(session_to_user(session.clone(), ip1));
-    dbg!(session_to_user(session, ip2));
+#[tokio::main]
+async fn main() {
+    let app = Router::new();
+
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
